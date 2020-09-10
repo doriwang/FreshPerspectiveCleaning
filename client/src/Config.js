36ade38fc8145234d505da0d5fb55b9e37a7cf6b
Brandon Gart <brandongart@gmail.com>
2020-09-10 14:39:54 -0600

    Adds env var for backend location in front end, option for "SAMEHOST"

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,5 +1,7 @@
 const config = {
-	BACKEND_HOST: "http://localhost:3001",
+	BACKEND_HOST: process.env.REACT_APP_BACKEND || "http://localhost:3001",
 }
+
+if (config.BACKEND_HOST === "SAMEHOST") { config.BACKEND_HOST = ""; }
 
 export default config;
